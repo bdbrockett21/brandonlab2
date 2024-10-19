788a5c71a8a2b04c277b53bb812ed37756820798
Braxx5286 <97646987+bdbrockett21@users.noreply.github.com>
2024-10-19 17:43:40 -0400

    Attempt 5b

--- time.c
+++ time.c
@@ -2,6 +2,8 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    
+    double startTimeMicro = start_time->tv_sec*1000000 + start_time->tv_usec; 
+    double endTimeMicro =end_time->tv_sec*1000000 + end_time->tv_usec;
+    return (endTimeMicro-startTimeMicro)/1000000.00;
     return 0.0;
 }
